76f962583049b9e4789acf85729f39258aa952a8
Aarne Kyppö <kyppoa@gmail.com>
2013-05-14 19:03:13 +0300

    Files for HiThere-server added to Git

---
+++ clientthread.py
@@ -0,0 +1,28 @@
+# encoding=utf-8
+from threading import Thread
+import socket
+class ClientThread(Thread):
+	def __init__(self, client,server, nick):
+		super(ClientThread,self).__init__()
+		self.nick = nick
+		self.client = client
+		self.server = server
+	def __del__(self):
+		print 'object deleted'
+	def run(self):
+		try:
+			while 1:
+				msg = self.client.recv(1024)
+				if not msg:
+					break
+				messages = msg[:-1].split(';') #User may get many messages at the same time
+				for m in messages:
+					#splitting message to type and message
+					print m
+					tmp = m.split(':')
+					type = tmp[0]
+					if type == 'msg': #Message to another client
+						self.server.sendMessage(tmp[1],self.nick,u'{0}'.format(tmp[2]))
+			self.server.deleteUser(self.nick)
+		except socket.error as mesg:
+			print mesg
